7ca61b9811317a0b45b0beb9573a33403780b191
Xyroinee-ID <123985221+Xyroinee@users.noreply.github.com>
2023-06-18 11:35:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,24 +27,48 @@ global.yt = "https://www.youtube.com/@Xyroinee"
 global.gh = "https://github.com/Xyroinee"
 global.ig = "https://instagram.com/danilelistz02"
 global.fb = "https://facebook.com/danilelistz19"
-global.web = "https://xyroinee.xyz"
-global.gcwa = "https://chat.whatsapp.com/DvcQaFvh9nL0TSGidAtdHc"
+global.web = "https://api.xyroinee.xyz"
+global.gcwa = "https://chat.whatsapp.com/LAWdMIQri9SKee5kqmISH0"
 
 // Foto Bot
 global.thumb = fs.readFileSync("./media/clara.jpg")
 
 // Message/Pesan
 global.mess = {
-    success: '_Berhasil Di Proses!',
-    admin: '_Maaf Kak, Tapi Fitur Ini Hanya Bisa Digunakan Oleh Admin Grup_',
-    botAdmin: '_Jadikan Saya Sebagai Admin Terlebih Dahulu Agar Dapan Mengaktifkan Fitur Ini_',
-    prem: '_Fitur Ini Khusus Premium Kak, Jika Ingin Menjadi User Premium Ketik .premium Atau Hubungin Owner_',
-    owner: '_Maaf Kak, Tapi Fitur Ini Hanya Bisa Digunakan Oleh Ownerku_',
-    group: '_Fitur Ini Hanya Bisa Di Pakai Di Dalam Grup_',
-    private: '_Fitur Ini Hanya Bisa Di Pakai Di Dalam Chat Pribadi_',
-    bot: 'Fitur Khusus Bot',
-    wait: '_Sedang Di Proses, Mohon Tunggu_...',
-    link: '_Uhm...Linknya Mana?',
+success: '_Sukses...!_',
+admin: '_Maaf Kak, Tapi Fitur Ini Hanya Bisa Digunakan Oleh Admin Grup_',
+botAdmin: '_Jadikan Saya Sebagai Admin Terlebih Dahulu Agar Dapan Mengaktifkan Fitur Ini_',
+prem: '_Fitur Ini Khusus Premium Kak, Jika Ingin Menjadi User Premium Ketik .premium Atau Hubungin Owner_',
+owner: '_Maaf Kak, Tapi Fitur Ini Hanya Bisa Digunakan Oleh Ownerku_',
+group: '_Fitur Ini Hanya Bisa Di Pakai Di Dalam Grup_',
+private: '_Fitur Ini Hanya Bisa Di Pakai Di Dalam Chat Pribadi_',
+bot: 'Fitur Khusus Bot',
+wait: '_Sedang Di Proses, Mohon Tunggu_...',
+link: '_Uhm...Linknya Mana?_'
+}
+// Ini Panel Ya Njing
+global.host = "https://zrstore.panelrun.xyz"
+global.application = {
+    api_key: "", //Ambil Di Panel Lu Caranya Pergi Ke Setting Admin Terus Tap Garis Tiga Pojok Kiri Atas Terus Tap Application API Tinggal Salin Apikeynya
+    c_api_key: "" //Ambil Di Panel Lu Caranya Ke Account Setting Atau Tap Foto Akun Lu Terus Tap API Credentials Terus Isi Descriptionya Sange Terus Tap Create Nah Lu Salin C Apikeynya
+}
+global.serverCreate = {
+    nestId: "5", //Nest Id Panel Lu
+    eggId: "15", //Egg Id Panel Lu
+    limits: {
+      db: "1", //Mending Ga Usah Diganti
+      backups: "1", //Mending Ga Usah Diganti
+      allocation: "0" //Mending Ga Usah Diganti
+    },
+    eggs: {
+      environment: {
+        "CUSTOM_COMMAND": "npm start", //Sesuaiin Sama Eggs Panel Lu
+        "GIT_ADDRESS": "",
+        "BRANCH": "",
+        "USERNAME": "",
+        "ACCESS_TOKEN": ""
+      }
+    }
 }
 
 let file = require.resolve(__filename)
